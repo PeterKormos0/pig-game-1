009c608398c8dfade5ef32430811d6e59232cff6
PeterKormos <110331177+PeterKormos0@users.noreply.github.com>
2022-12-14 10:25:46 +0100

    set starter values to zero

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Selecting elements
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById("score--1");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
