f01a123eae9229f53e82749b4006477fd0e1adab
PeterKormos <110331177+PeterKormos0@users.noreply.github.com>
2022-12-14 10:37:08 +0100

    Started to work on Rolling dice functionality

--- script.js
+++ script.js
@@ -3,7 +3,15 @@
 // Selecting elements
 let score0El = document.querySelector('#score--0');
 let score1El = document.getElementById("score--1");
+let diceEl = document.querySelector('.dice');
+let btnNew = document.querySelector('.btn--new');
+let btnNew = document.querySelector('.btn--roll');
+let btnNew = document.querySelector('.btn--hold');
 
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+// Rolling dice functionality
 
