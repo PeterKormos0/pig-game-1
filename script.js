98ed892ecdb03382a184780ad554ee9e10635830
PeterKormos <110331177+PeterKormos0@users.noreply.github.com>
2022-12-14 10:52:25 +0100

    random dice displayed

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let score0El = document.querySelector('#score--0');
 let score1El = document.getElementById("score--1");
 let diceEl = document.querySelector('.dice');
 let btnNew = document.querySelector('.btn--new');
-let btnNew = document.querySelector('.btn--roll');
-let btnNew = document.querySelector('.btn--hold');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
@@ -14,4 +14,14 @@ score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
 // Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+    // 1. Generating a random dice roll
+    let dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+})
 
